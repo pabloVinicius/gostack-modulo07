a75b21dce7f183cc53a57d35cf510d723f5b2867
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-15 11:33:50 -0300

    main project configuration

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Home, Cart } from './pages';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+};
+
+export default Routes;
