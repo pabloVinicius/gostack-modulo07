161dc9a1f1879d63b97731f48f7eb28867f77cbc
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-15 15:58:21 -0300

    styling cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(90)/adaption/catalog/kits/2-pares/preto-cinza.jpg"
+                alt="Tênis"
+              />
+            </td>
+            <td>Meu tênis</td>
+            <td>R$ 80,00</td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={20} />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 500,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 };
 
 export default Cart;
