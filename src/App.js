c281a0970e7e71eba48ee708aa3550810ceb1e07
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-15 11:49:03 -0300

    styling header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyles from './styles/global';
+import { Header } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
+      <Header />
       <Routes />
     </BrowserRouter>
   );
